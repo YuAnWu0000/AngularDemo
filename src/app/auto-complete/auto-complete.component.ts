9faeb0ed7a7501ecbccdb202d461aae84cdc09a8
unknown <andywu03242000@gmail.com>
2018-11-19 10:37:55 +0800

    service dependency injection

--- src/app/auto-complete/auto-complete.component.ts
+++ src/app/auto-complete/auto-complete.component.ts
@@ -11,45 +11,25 @@ import { AutoCompleteService } from './auto-complete.service';
   providers: [AutoCompleteService]
 })
 export class AutoCompleteComponent implements OnInit, AfterViewInit {
-
-  public getSuggestList(term){
-    return $.ajax({
-      url: 'http://en.wikipedia.org/w/api.php',
-      dataType: 'jsonp',
-      data: {
-          action: 'opensearch',
-          format: 'json',
-          search: term
-      }
-    }).promise();
-  }
-  public renderList (list) {
-    $('#suggest-list').empty();
-    $('#suggest-list').append(list.map(item => '<li>' + item + '</li>'));
-  }
-  // public searchInput = document.getElementById('search');
-  // public suggestList = document.getElementById('suggest-list');
-  // public keyword$: Observable<any> = fromEvent(this.searchInput, 'input');
-  // public selectItem$: Observable<any> = fromEvent(this.suggestList, 'click');
+  
   public keyword;
   public selectItem;
-  constructor() { }
+  constructor(private autoCompleteService: AutoCompleteService) { }
 
   ngOnInit() { }
   ngAfterViewInit(){
-    //this.keyword$.pipe(map(e => e.target.value)).pipe(switchMap(e => this.getSuggestList(e))).subscribe(console.log);
     this.keyword = fromEvent(document.querySelector('#search'), 'input')
                   .pipe(
                         filter(e => e.target['value'].length > 0),
                         debounceTime(100),
                         map(e => e.target['value']),
                         switchMap(value => {
-                          return from(this.getSuggestList(value))
+                          return from(this.autoCompleteService.getSuggestList(value))
                         }),
                         map(res => res[1])
                         )
                   .subscribe(value => {
-                    this.renderList(value);
+                    this.autoCompleteService.renderList(value);
                   })
     this.selectItem = fromEvent(document.querySelector('#suggest-list'), 'click')
                      .pipe(
@@ -58,7 +38,7 @@ export class AutoCompleteComponent implements OnInit, AfterViewInit {
                      )
                      .subscribe(text => {
                       (<HTMLTextAreaElement>document.querySelector('#search')).value = text;
-                      this.renderList([]);
+                      this.autoCompleteService.renderList([]);
                      })
   }
   
--- src/app/counter/counter.service.ts
+++ src/app/counter/counter.service.ts
@@ -8,14 +8,17 @@ export class CounterService {
   countDown$: Observable<any>;
   constructor() { 
     this.countDown$ = Observable.create((observer: Observer<any>)=>{
-      for(let i=0; i<100; i++){
-        setTimeout(()=>{
-          observer.next(10 - i);
-          if(i === 10){ //如果還沒到10就會繼續丟next，即使頁面跳轉(CounterComponent已經Destroy)也一樣
-            observer.complete(); 
-          }
-        }, i * 1000);
-      }
+      // for(let i=0; i<100; i++){
+      //   setTimeout(()=>{
+      //     observer.next(10 - i);
+      //     if(i === 10){ //如果還沒到10就會繼續丟next，即使頁面跳轉(CounterComponent已經Destroy)也一樣
+      //       observer.complete(); 
+      //     }
+      //   }, i * 1000);
+      // }
+      let count = 10;
+      observer.next(count--);
+      setInterval(()=>observer.next(count--), 1000);
     })
   }
   
